e7a5b8ffad84827e7b960997b47b77fd0f7b1531
Vinay <sherson0402@gmail.com>
2025-06-06 23:22:16 +0530

    First

---
+++ lowerTriangle.py
@@ -0,0 +1,5 @@
+rows = 5  # You can change this value for a larger or smaller triangle
+
+for i in range(1, rows + 1):
+    print('*' * i)
+    
---
+++ pyramid.py
@@ -0,0 +1,8 @@
+def pyramid(height):
+    for i in range(1, height + 1):
+        spaces = ' ' * (height - i)
+        stars = '*' * (2 * i - 1)
+        print(spaces + stars + spaces)
+
+height = int(input("Enter the height of the pyramid: "))
+pyramid(height)
